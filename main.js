0bfde8d2dec2c7b47d2ab32b734a7cf3908c0f4f
scratchis <103792439+scratchis@users.noreply.github.com>
2023-06-22 18:15:47 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -41,4 +41,30 @@ function setup(){
     canvas=createCanvas(400,400)
     canvas.center();
     background("white")
+    classifyCanvas()
+}
+
+// Code to Remove if needed
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed) {
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    }
+    console.log(results);
+    document.getElementById('sketch_drawn').innerHTML = 'Sketch Drawn: ' + results[0].label;
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
 }
